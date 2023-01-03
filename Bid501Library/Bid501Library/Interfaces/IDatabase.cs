1b3cf7c5760754234b08ac6777a5074464eaaa52
SebTrill <89354578+SebTrill@users.noreply.github.com>
2023-01-03 17:02:48 -0600

    Added Library

---
+++ Bid501Library/Bid501Library/Interfaces/IDatabase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bid501Library.Interfaces
+{
+    public interface IDatabase
+    {
+        /// <summary>
+        /// This sends the Product List of the product from the server to the client or vice-versa.
+        /// </summary>
+        /// <returns>This is the Product List to send.</returns>
+        public List<IProduct> SendProductList();
+
+        /// <summary>
+        /// This sends the Credentials List of the product from the server to the client or vice-versa.
+        /// </summary>
+        /// <returns>This is the Credentials List to send.</returns>
+        public List<string> SendCredentialsList();
+
+        /// <summary>
+        /// This updates the current Product List.
+        /// </summary>
+        public void UpdateProductList();
+    }
+}
---
+++ Bid501Library/Bid501Library/Interfaces/IProduct.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bid501Library.Interfaces
+{
+    public interface IProduct
+    {
+        /// <summary>
+        /// This sends the Name of the product from the server to the client or vice-versa.
+        /// </summary>
+        /// <returns>This is the Name to send.</returns>
+        public string SendName();
+
+        /// <summary>
+        /// This sends the Price of the product from the server to the client or vice-versa.
+        /// </summary>
+        /// <returns>This is the Price to send.</returns>
+        public double SendPrice();
+
+        /// <summary>
+        /// This sends the Description of the product from the server to the client or vice-versa.
+        /// </summary>
+        /// <returns>This is the Description to send.</returns>
+        public string SendDescription();
+
+        /// <summary>
+        /// This sends the ID of the product from the server to the client or vice-versa.
+        /// </summary>
+        /// <returns>This is the ID to send.</returns>
+        public int SendID();
+
+        /// <summary>
+        /// This sends the Status of the product from the server to the client or vice-versa.
+        /// </summary>
+        /// <returns>This is the Status to send.</returns>
+        public bool SendStatus();
+    }
+}
